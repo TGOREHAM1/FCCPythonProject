21f33d6e50b8c27582544c2583fafdf7f2c110ee
TGOREHAM1 <tomgoreham@hotmail.co.uk>
2025-05-05 19:12:54 +0200

    variables wasn't up to date

---
+++ variabletest.py
@@ -0,0 +1,7 @@
+char_name = "Alpharius"
+char_age = "108"
+print("There once was a man named " + char_name + ",")
+print("He was " + char_age + " years old.")
+
+char_name = "Pech"
+print("He was actually called " + char_name + ".")
