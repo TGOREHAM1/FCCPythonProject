100b8cadd706ffc385ff9b2628abab5de17075f4
TGOREHAM1 <tomgoreham@hotmail.co.uk>
2025-05-06 20:44:07 +0200

    big name improvement

--- variabletest.py
+++ variabletest.py
@@ -1,7 +1,7 @@
 char_name = "Alpharius"
-char_age = "108"
+char_age = "200"
 print("There once was a man named " + char_name + ",")
 print("He was " + char_age + " years old.")
 
-char_name = "Pech"
+char_name = "Omegon"
 print("He was actually called " + char_name + ".")
