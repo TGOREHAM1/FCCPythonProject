f4d1df25a651fdadb26c4173f501aa3f98ec9a83
TGOREHAM1 <tomgoreham@hotmail.co.uk>
2025-05-08 22:17:10 +0200

    converted int to float to allow for cubing of non-whole numbers

--- cuber.py
+++ cuber.py
@@ -2,9 +2,9 @@ def cube():
     while True:
         num = input("Input a number to cube: ")
         try:
-            num_int = int(num)
-            result = num_int*num_int*num_int
-            print(str(result))
+            num_float = float(num)
+            result = num_float ** 3
+            print("Result of " + num + " cubed: " + str(result))
             break
         except ValueError:
             print("That's not a number. Try again.")
