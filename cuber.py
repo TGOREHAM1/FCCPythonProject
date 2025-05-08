a161544dfc18b54b879cdbbe44b545c69e67ce1c
TGOREHAM1 <tomgoreham@hotmail.co.uk>
2025-05-08 22:02:25 +0200

    cleaned it up a bit

--- cuber.py
+++ cuber.py
@@ -1,13 +1,6 @@
-def cube(num):
-    num_int = int(num)
-    cubed_num = num_int*num_int*num_int
-    print("Cubed number: " + str(cubed_num))
-
-cube(input("Input a number to cube: "))
-
-def efficient_cube():
+def cube():
     while True:
-        num = input("Input a number to EFFICIENTLY cube: ")
+        num = input("Input a number to cube: ")
         try:
             num_int = int(num)
             result = num_int*num_int*num_int
@@ -16,4 +9,4 @@ def efficient_cube():
         except ValueError:
             print("That's not a number. Try again.")
 
-efficient_cube()
+cube()
